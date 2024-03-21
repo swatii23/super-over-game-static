2ceeda4f5c2e6cb9b24cca6bddd82833dc15aa4e
swatii23 <swati.m_2000@rediffmail.com>
2024-03-21 16:12:27 +0530

    declaring winner

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const strikeButton = document.getElementById("strike");
-const resetButton = document.getElementById("reset");
-const score1 = document.getElementById("score-team1");
-const score2 = document.getElementById("score-team2");
-const wicket1 = document.getElementById("wickets-team1");
-const wicket2 = document.getElementById("wickets-team2");
-console.log("superover")
+let strikeButton = document.getElementById("strike");
+let resetButton = document.getElementById("reset");
+let score1 = document.getElementById("score-team1");
+let score2 = document.getElementById("score-team2");
+let wicket1 = document.getElementById("wickets-team1");
+let wicket2 = document.getElementById("wickets-team2");
+console.log(resetButton)
 var scoret1 = 0;
 var scoret2 = 0;
 var wickett1 = 0;
@@ -17,29 +17,49 @@ var team2ball = 0;
 var possibility = [0, 1, 2, 3, 4, 6, "W"]
 
 strikeButton.addEventListener("click",() => {
-    console.log("inside click");
+    
     random = possibility[Math.floor(Math.random() * possibility.length)];
 
     if(turn == 1){
         team1ball++;
 
         if(random == 'W')
-        wicket1++;
+        wickett1++;
         
         else
         scoret1 += random;
+
+        document.querySelector(
+            `#team1-superover div:nth-child(${team1ball})`)
+            .textContent = random;
+            
+        if(team1ball == 6){
+            turn = 2;
+            team1ball = 0;
+            return;
+        }
     }
+    
     if(turn == 2){
         team2ball++;
 
         if(random == 'W')
-        wicket2++;
+        wickett2++;
         
         else
         scoret2 += random;
+
+        document.querySelector(
+            `#team2-superover div:nth-child(${team2ball})`)
+            .textContent = random;
+        if(team2ball == 6){
+            team2ball = 0;
+            decision();
+        }
     }
-
-
+     
+    
+    update();  
 })
 
 let update = () => {
@@ -48,9 +68,21 @@ let update = () => {
     wicket1.textContent = wickett1;
     wicket2.textContent = wickett2;
     let team = document.querySelector(`team${turn}-superover > div:nth-child(${turn == 1 ? team1ball : team2ball})`);
-    team.textContent = turn == 1 ? scoret1 : scoret2;
+    console.log(team);
+    team.textContent = random;
 }
 
-resetButton.onclick(() => {
+let decision = () => {
+    if(scoret1 > scoret2)
+        alert("IND Wins!")
+    else if(scoret2 > scoret1)
+        alert("PAK Wins!")
+    else{
+        alert("Play Again!");
+        window.location.reload();
+    }
+}
+
+resetButton.addEventListener("click",() => {
     window.location.reload();
 })
